695425d252b2ca804a88bfdd085b176d5e655b13
tduki <95369488+tduki@users.noreply.github.com>
2022-03-11 11:55:34 +0100

    Add files via upload

---
+++ dc_exo1.plantulm.groovy
@@ -0,0 +1,39 @@
+@startuml dc
+
+skinparam  classAttributeIconSize  0
+
+masquer le  cercle
+masquer les  membres vides 
+masquer les  méthodes vides 
+
+Entreprise  "1"  -- "*" Artisan : gerer
+Entreprise  "1"  -- "*" Chantier : possède
+Chantier  "*"  -- "*" Artisan 
+( Artisan , Chantier ) ..  affectant
+
+Classe  Entreprise {
+    nom
+    adresse
+    nbEmployés
+}
+
+ Artisan de classe {
+    nom
+    coordonnées
+    spécialité
+}
+
+ Chantier de classe {
+    adresse
+    dateDébut
+    dateFin
+}
+
+Classe  affectant {
+    nombreHeures
+    débutMission
+    finMission
+    getPayeArtisan ()
+}
+
+@enduml
